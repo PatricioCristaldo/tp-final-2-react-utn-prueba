e45fbc7771ab21bed4ecf335b3a796a59d3a8b99
Patricio <patricio_cristaldo@outlook.com>
2024-07-28 12:24:44 -0300

    Agrego fuentes y estilos

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './normalize.css'
 import Header from './Components/Header/Header'
 import Main from './Components/Main/Main'
 import Footer from './Components/Footer/Footer'
