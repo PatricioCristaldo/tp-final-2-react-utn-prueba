e38a35fc430b12dad46ef4e17ae2af1d0b3804c1
Patricio <patricio_cristaldo@outlook.com>
2024-07-30 22:35:43 -0300

    Cambios deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tp-final-2-react-utn",
   plugins: [react()],
-})
+  base: '/tp-final-2-react-utn/', // Ajusta esto al nombre de tu repositorio
+});
