39fafdb19d0af1d511de3504ab44859737a29d71
Patricio <patricio_cristaldo@outlook.com>
2024-07-30 23:02:19 -0300

    a

---
+++ dist/src/pages/index/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import './normalize.css';
+import Header from '../../Components/Header/Header';
+import Main from '../../Components/Main/Main';
+import Footer from '../../Components/Footer/Footer';
+import About from '../about/About';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Main></Main>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
